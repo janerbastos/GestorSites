d5fe7492418c1eb09e431c6f64b01b3e7d4bc917
Janer Bastos De Melo <janer.melo@ufopa.edu.br>
2017-03-14 16:57:09 -0300

    Configuração da base de dados para mysql

--- GestorSites/settings.py
+++ GestorSites/settings.py
@@ -81,9 +81,18 @@ WSGI_APPLICATION = 'GestorSites.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portal_ufopa',
+        'USER': 'mamba_negra',
+        'PASSWORD': 'mamba',
+        'HOST': '10.3.9.31',
+        'PORT': '3306',
     }
 }
 
