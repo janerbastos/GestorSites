77330b0ef4b2c0c9815e4cd4bddee3787f37c230
Janer B. Melo <janerbastos@gmail.com>
2017-03-25 17:18:22 -0300

    Correção dos imports

--- portalufopa/comum/paginas.py
+++ portalufopa/comum/paginas.py
@@ -8,6 +8,7 @@ from ..models import Pagina
 from ..comum.contents import reescrever_url, save_in_portal_catalog, get_site_url
 from ..comum.contents import get_site_url_id, get_url_id_content
 from portalufopa.comum.contents import save_indice_url
+from portalufopa.models import PortalCatalog
 
 
 TEMPLATE = '%s/documents.html' % 'comum'
@@ -61,7 +62,16 @@ def delete(request, portal_catalog):
     portal_catalog.delete()
     _new_url = reescrever_url(request)
     _new_url = _new_url.replace('/'+content_url, '')
-
+    #verica ocorrencia de visão do content na pasta e corrige
+    _site_url = get_site_url_id(request)
+    try:
+        pc = PortalCatalog.objects.filter(site__url=_site_url).get(path_url=_new_url)
+        pasta = pc.get_content_object()
+        if pasta.visao_padrao not in ('sumaria', 'agenda', 'evento', 'arquivo', 'noticia', None):
+            pasta.visao_padrao='sumaria'
+            pasta.save()
+    except:
+        pass
     return redirect(_new_url)
 
 def workflow(request, portal_catalog, _workflow):
