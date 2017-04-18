4358bfef27560eeb039f25e4ad06b531f5fafab3
Servio Santos <servio.santos@ufopa.edu.br>
2017-04-18 10:57:57 -0300

    ajustando o comum js

--- portalufopa/static/js/comum.js
+++ portalufopa/static/js/comum.js
@@ -1,37 +1,6 @@
-
 $(document).ready(function () {
-// menu hover
 
-  if(screen.width > 767){  
-    $('ul.nav li.dropdown').hover(function() {
-      $(this).find('.dropdown-menu1').stop(true, true).delay(0).fadeIn(50);
-        }, function() {
-        $(this).find('.dropdown-menu1').stop(true, true).delay(0).fadeOut(50);
-      });
-
-    // fix lateral comum
-    var alturaPaginas = document.getElementById('conteudo-paginas').clientHeight;
-    var alturaLateral = document.getElementById('lateral').clientHeight; 
-    if(alturaPaginas > alturaLateral){  
-      $('#lateral').affix({
-        offset: {     
-          top: 450,
-          bottom:  $('.rodape').outerHeight(true)
-        }
-      });
-    }
-  }
-
-// fim meunu hover
-  
-// alto-contraste
-  $("body").on('click', '.ativa-contraste', function(event) {
-    event.preventDefault();
-    /* Act on the event */
-  $('body').toggleClass('contraste');
-  });
-  
-// aumentando a fonte
+  // aumentando a fonte
   $(".inc-font").click(function () {
     var size = $("body").css('font-size');
     size = size.replace('px', '');
@@ -39,7 +8,7 @@ $(document).ready(function () {
     $("body").animate({'font-size' : size + 'px'});
   });
 
-//diminuindo a fonte
+  //diminuindo a fonte
   $(".dec-font").click(function () {
     var size = $("body").css('font-size');
     size = size.replace('px', '');
@@ -47,15 +16,50 @@ $(document).ready(function () {
     $("body").animate({'font-size' : size + 'px'});
   });
 
-// resetando a fonte
+  // resetando a fonte
   $(".res-font").click(function () {
-    $("body").animate({'font-size' : '14px'});
+    $("body").animate({'font-size' : '10px'});
   });
-//nano 
+
+  // agenda e ultimas noticias
   $(".nano").nanoScroller();
   $(".nanoAgenda").nanoScroller();
   
-//Mneu fixo topo
+  // busca
+    $("#topo").on('click', '.btn-search', function(event) {
+      event.preventDefault();
+      $("body").addClass('search-active');
+    });
+    $(".search-popup-bg").on('click', function(event) {
+      $("body").removeClass('search-active');
+    });
+    // contrast ativa
+  $("body").on('click', '.ativa-contraste', function(event) {
+    event.preventDefault();
+    
+  $('body').toggleClass('contraste');
+  });
+
+  // menu hover
+
+  if(screen.width > 767){  
+    $('ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu1').stop(true, true).delay(0).fadeIn(50);
+        }, function() {
+        $(this).find('.dropdown-menu1').stop(true, true).delay(0).fadeOut(50);
+      });
+    // menu lateral fixo
+  $('#lateral').affix({
+    offset: {     
+      top: 605,
+      bottom:  $('#rodape').outerHeight(true)
+    }
+  });
+  }
+
+  // fim meunu hover
+});
+// menu fixo
   $(document).ready(function() {
       var navpos = $('#menu').offset();
       console.log(navpos.top);
@@ -69,7 +73,6 @@ $(document).ready(function () {
           $('#menu').removeClass('navbar-fixed-top');
           $('#menu').css('border-bottom','0px');
           $('#destaque').css('margin-top','30px');
-
         }
 
         $("#linksmenu").on('click',function(e) {
@@ -80,23 +83,22 @@ $(document).ready(function () {
 
   });
 
-
-  // busca
-    $("#topo").on('click', '.btn-search', function(event) {
-      event.preventDefault();
-      $("body").addClass('search-active');
-    });
-    $(".search-popup-bg").on('click', function(event) {
-      $("body").removeClass('search-active');
-    });
-
   // redes sociais
-  // facebook
-              (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-});
+     // facebook
+     (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+     
+     //twitter
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
+      p=/^http:/.test(d.location)?'http':'https';
+      if(!d.getElementById(id)){js=d.createElement(s);
+        js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js,fjs);
+      }}(document, 'script', 'twitter-wjs');
+
+  
